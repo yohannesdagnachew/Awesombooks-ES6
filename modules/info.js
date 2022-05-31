9df65b461635369568aa6ccc3d4a2224bbe4edc7
yohannesdagnachew <102184682+yohannesdagnachew@users.noreply.github.com>
2022-05-31 13:26:57 +0300

    Fix linter

--- modules/info.js
+++ modules/info.js
@@ -1,6 +1,6 @@
 export default class Info {
-    constructor(title, author) {
-      this.title = title;
-      this.author = author;
-    }
-  }
+constructor(title, author) {
+  this.title = title;
+  this.author = author;
+}
+}
